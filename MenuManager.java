ddd74e8fecaaa89c688f049b276e0f9a9bd5be8a
zzidong <106225011+zzidong@users.noreply.github.com>
2023-03-23 15:25:05 +0900

    Create MenuManager.java

---
+++ MenuManager.java
@@ -0,0 +1,19 @@
+//경상국립대학교 객체지향언어 정지원 2022210316
+
+package research;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("add schedule");
+		System.out.println("delete schedule");
+		System.out.println("edit schedule");
+		System.out.println("view schedule");
+		System.out.println("exit");
+
+		
+		
+
+}
+}
